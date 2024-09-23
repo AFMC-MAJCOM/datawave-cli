16dfa78cbe953db993b1c3692984da2f8d329603
David Lambertson <david.lambertson@opialab.com>
2024-09-23 10:09:45 -0600

    fixing stuff

--- src/datawave_cli/accumulo_interactions.py
+++ src/datawave_cli/accumulo_interactions.py
@@ -1,94 +1,58 @@
-import argparse
-import click
 import logging
-import os
-import requests
 import sys
 from types import SimpleNamespace
-from datetime import datetime
+
+import click
+import requests
+
+from .base_interactions import BaseInteractions
 from datawave_cli.utilities import pods
 from datawave_cli.utilities.cli_stuff import common_options
 from datawave_cli.utilities.utilities import setup_logger, log_http_response
 
 
-base_url = f"datawave.{os.environ.get('DWV_URL', '')}"
+class AccumuloInteractions(BaseInteractions):
+    def __init__(self, args, log: logging.Logger = logging.getLogger('accumulo_interactions')):
+        self.log = log
+        super().__init__(args)
 
 
-def reload_accumulo_cache(cert: str, namespace: str,
-                          log: logging.Logger = logging.getLogger('accumulo_interactions'),
-                          use_ip: bool = False):
-    """
-    Perform the accumulo cache reload within DataWave.
+    def get_pod_ip(self):
+        return pods.get_specific_pod(pods.web_datawave_info, self.namespace).pod_ip
 
-    Parameters
-    ----------
-    cert: str
-        the cert to use in the HTTP Request.
-    namespace: str
-        the kubernetes namespace to get information from.
-    log: logging.Logger
-        The logger object.
-    use_ip: bool
-        Enables using the IP and Port instead of DNS
-    """
-    log.info("Reloading the accumulo cache...")
-    if use_ip:
-        pod_ip = pods.get_specific_pod(pods.web_datawave_info, namespace).pod_ip
-        request = f"https://{pod_ip}:8443/DataWave/Common/AccumuloTableCache/reload/datawave.metadata"
-    else:
-        request = f"https://{base_url}/DataWave/Common/AccumuloTableCache/reload/datawave.metadata"
-    resp = requests.get(request, cert=cert, verify=False)
-    log_http_response(resp, log)
-    log.info("Successfully requested a reload.")
+    def reload_accumulo_cache(self):
+        """
+        Perform the accumulo cache reload within DataWave.
+        """
+        self.log.info("Reloading the accumulo cache...")
+        request = f"{self.base_url}/DataWave/Common/AccumuloTableCache/reload/datawave.metadata"
+        resp = requests.get(request, cert=self.cert, verify=False)
+        log_http_response(resp, self.log)
+        self.log.info("Successfully requested a reload.")
 
+    def view_accumulo_cache(self):
+        """
+        View what is happening with the accumulo cache in DataWave.
 
-def view_accumulo_cache(cert: str, namespace: str,
-                        log: logging.Logger = logging.getLogger('accumulo_interactions'),
-                        use_ip: bool = False):
-    """
-    View what is happening with the accumulo cache in DataWave.
-
-    Parameters
-    ----------
-    cert: str
-        the cert to use in the HTTP Request.
-    namespace: str
-        the kubernetes namespace to get information from.
-    log: logging.Logger
-        The logger object.
-    use_ip: bool
-        Enables using the IP and Port instead of DNS
-
-    RETURNS
-    -------
-    Response text from the HTTP request
-    """
-    log.info("Viewing the accumulo cache...")
-    if use_ip:
-        pod_ip = pods.get_specific_pod(pods.web_datawave_info, namespace).pod_ip
-        request = f"https://{pod_ip}:8443/DataWave/Common/AccumuloTableCache/"
-    else:
-        request = f"https://{base_url}/DataWave/Common/AccumuloTableCache/"
-    resp = requests.get(request, cert=cert, verify=False)
-    log_http_response(resp, log)
-    return resp.text
+        RETURNS
+        -------
+        Response text from the HTTP request
+        """
+        self.log.info("Viewing the accumulo cache...")
+        request = f"{self.base_url}/DataWave/Common/AccumuloTableCache/"
+        resp = requests.get(request, cert=self.cert, verify=False)
+        log_http_response(resp, self.log)
+        return resp.text
 
 
 def main(args):
     log = setup_logger("accumulo_interactions", log_level=args.log_level)
-    global base_url
-    base_url = f"datawave.{args.url}"
-
-    if args.key is None:
-        cert = args.cert
-    else:
-        cert = (args.cert, args.key)
-
+    ai = AccumuloInteractions(args, log=log)
     if args.view:
-        text = view_accumulo_cache(cert, args.namespace, log=log, use_ip=args.ip)
+        text = ai.view_accumulo_cache()
         log.info(text)
     else:
-        reload_accumulo_cache(cert, args.namespace, log=log, use_ip=args.ip)
+        ai.reload_accumulo_cache()
 
 
 @click.command
--- src/datawave_cli/dictionary_interactions.py
+++ src/datawave_cli/dictionary_interactions.py
@@ -1,12 +1,12 @@
-import argparse
-import click
-import json
 import logging
 import os
-import requests
 import sys
 from types import SimpleNamespace
-from datetime import datetime
+
+import click
+import requests
+
+from .base_interactions import BaseInteractions
 from datawave_cli.utilities import pods
 from datawave_cli.utilities.cli_stuff import File, common_options
 from datawave_cli.utilities.utilities import setup_logger, log_http_response
@@ -14,156 +14,148 @@ from datawave_cli.utilities.utilities import setup_logger, log_http_response
 
 base_url = f"dwv-dictionary.{os.environ.get('DWV_URL', '')}"
 
+class DictionaryInteractions(BaseInteractions):
+    def __init__(self, args, log: logging.Logger = logging.getLogger('dictionary_interactions')):
+        self.log = log
+        super().__init__(args)
 
-def get_entire_dictionary(cert: str, auths: str, data_types: str, namespace: str, file: str,
-                          log: logging.Logger = logging.getLogger('dictionary_interactions'),
-                          use_ip: bool = False):
-    """
-    Display or save the entire dictionary of datawave, all data types.
+    def get_pod_ip(self):
+        return pods.get_specific_pod(pods.web_dictionary_info, self.namespace).pod_ip
 
-    Parameters
-    ----------
-    cert: str
-        the cert to use in the HTTP Request.
-    namespace: str
-        the kubernetes namespace to get information from.
-    file: str
-        the name of where to save output
-    log: logging.Logger
-        the log object.
-    use_ip: bool
-        Enables using the IP and Port instead of DNS
-    """
-    log.info("Getting the entire field dictionary in DataWave...")
-    if use_ip:
-        pod_ip = pods.get_specific_pod(pods.web_dictionary_info, namespace).pod_ip
-        request = f"https://{pod_ip}:8443/dictionary/data/v1/"
-    else:
-        request = f"https://{base_url}/dictionary/data/v1/"
-    data = {'auths': auths,
-            'dataTypeFilters': data_types}
-    resp = requests.get(request, data=data, cert=cert, verify=False)
-    log_http_response(resp, log)
-    fields = parse_response(resp)
-    if file is None:
-        display_dictionary(fields, log)
-    else:
-        save_dictionary(file, fields, log)
-    return fields
+    def get_entire_dictionary(self, auths: str, data_types: str, file: str):
+        """
+        Display or save the entire dictionary of datawave, all data types.
 
+        Parameters
+        ----------
+        auths: str
+            comma delimited string containing all the auths to pull the dictionary for
+        data_types: str
+            comma delimited string comtaining the data types to pull the dictionary for
+        file: str
+            the name of where to save output
+        """
+        self.log.info("Getting the entire field dictionary in DataWave...")
+        request = f"{self.base_url}/dictionary/data/v1/"
+        data = {'auths': auths,
+                'dataTypeFilters': data_types}
+        resp = requests.get(request, data=data, cert=self.cert, verify=False)
+        log_http_response(resp, self.log)
+        if resp.status_code == 200:
+            fields = self.parse_response(resp)
+            if file is None:
+                self.display_dictionary(fields, self.log)
+            else:
+                self.save_dictionary(file, fields, self.log)
+            return fields
+        else:
+            return {}
 
-def parse_response(resp: requests.Response) -> dict:
-    """
-    Rips apart the HTTP Response content and pulls of the information we desire.
+    def parse_response(self, resp: requests.Response) -> dict:
+        """
+        Rips apart the HTTP Response content and pulls of the information we desire.
 
-    Parameters
-    ----------
-    resp: requests.Response
-        the HTTP Response object.
+        Parameters
+        ----------
+        resp: requests.Response
+            the HTTP Response object.
 
-    Returns
-    -------
-    A dictionary containing all the information parsed out of the HTTP JSON response.
-    """
-    fields = []
-    for field in resp.json()['MetadataFields']:
-        field_name = field['fieldName']
-        field_data_type = field['dataType']
-        field_forward_indexed = field['forwardIndexed']
-        field_reverse_indexed = field['reverseIndexed']
-        field_types = field['Types']
-        field_description = field['Descriptions']
-        field_index_only = field['indexOnly']
-        field_normalized = field['normalized']
-        field_tokenized = field['tokenized']
-        field_last_updated = field['lastUpdated']
+        Returns
+        -------
+        A dictionary containing all the information parsed out of the HTTP JSON response.
+        """
+        fields = []
+        for field in resp.json()['MetadataFields']:
+            field_name = field['fieldName']
+            field_data_type = field['dataType']
+            field_forward_indexed = field['forwardIndexed']
+            field_reverse_indexed = field['reverseIndexed']
+            field_types = field['Types']
+            field_description = field['Descriptions']
+            field_index_only = field['indexOnly']
+            field_normalized = field['normalized']
+            field_tokenized = field['tokenized']
+            field_last_updated = field['lastUpdated']
 
-        fields.append({"name": field_name, "Data Type": field_data_type, "Forward Indexed": field_forward_indexed,
-                       "Reversed Indexed": field_reverse_indexed, "Types": field_types,
-                       "Tokenized": field_tokenized, "Normalized": field_normalized, "Index Only": field_index_only,
-                       "Descriptions": field_description, "Last Updated": field_last_updated})
-    return fields
+            fields.append({"name": field_name, "Data Type": field_data_type, "Forward Indexed": field_forward_indexed,
+                        "Reversed Indexed": field_reverse_indexed, "Types": field_types,
+                        "Tokenized": field_tokenized, "Normalized": field_normalized, "Index Only": field_index_only,
+                        "Descriptions": field_description, "Last Updated": field_last_updated})
+        return fields
 
 
-def display_dictionary(fields: list,
-                       log: logging.Logger = logging.getLogger('dictionary_interactions')):
-    """
-    Displays a dictionary of all the fields found from the query.
+    def display_dictionary(self, fields: list,
+                        log: logging.Logger = logging.getLogger('dictionary_interactions')):
+        """
+        Displays a dictionary of all the fields found from the query.
 
-    Parameters
-    ----------
-    fields: list
-        the fields returned from the filter function.
-    log: logging.Logging
-        The logger object.
-    """
-    if not fields:
-        log.warning("No fields to display.")
-        return
-    dictionary_keys = fields[0].keys()
-    max_lengths = {}
-    # Build Header
-    header = ""
-    row_split = ""
-    for key in dictionary_keys:
-        max_lengths[key] = max(max([len(str(f[key])) for f in fields]), len(key))
-        header += f"{key:{max_lengths[key]}}|"
-        row_split += "-" * max_lengths[key] + "|"
-    log.info(header)
-    log.info(row_split)
-    for field in fields:
-        row = ""
+        Parameters
+        ----------
+        fields: list
+            the fields returned from the filter function.
+        log: logging.Logging
+            The logger object.
+        """
+        if not fields:
+            log.warning("No fields to display.")
+            return
+        dictionary_keys = fields[0].keys()
+        max_lengths = {}
+        # Build Header
+        header = ""
+        row_split = ""
         for key in dictionary_keys:
-            row += f"{str(field[key]):{max_lengths[key]}}|"
-        log.info(row)
-
-
-def save_dictionary(filename: str, fields: list,
-                    log: logging.Logger = logging.getLogger('dictionary_interactions')):
-    """
-    Saves a dictionary of all the fields found from the query.
-
-    Parameters
-    ----------
-    filename: str
-        the name where to save the dictionary output
-    fields: list
-        the fields returned from the filter function.
-    log: logging.Logger
-        the log object.
-    """
-    if not fields:
-        log.warning("No fields to display.")
-        return
-    dictionary_keys = fields[0].keys()
-    max_lengths = {}
-    header = ""
-    row_split = ""
-    for key in dictionary_keys:
-        max_lengths[key] = max(max([len(str(f[key])) for f in fields]), len(key))
-        header += f"{key:{max_lengths[key]}}|"
-        row_split += "-" * max_lengths[key] + "|"
-    with open(filename, 'a') as file:
-        print(header, file=file)
-        print(row_split, file=file)
+            max_lengths[key] = max(max([len(str(f[key])) for f in fields]), len(key))
+            header += f"{key:{max_lengths[key]}}|"
+            row_split += "-" * max_lengths[key] + "|"
+        log.info(header)
+        log.info(row_split)
         for field in fields:
             row = ""
             for key in dictionary_keys:
                 row += f"{str(field[key]):{max_lengths[key]}}|"
-            print(row, file=file)
+            log.info(row)
+
+
+    def save_dictionary(self, filename: str, fields: list,
+                        log: logging.Logger = logging.getLogger('dictionary_interactions')):
+        """
+        Saves a dictionary of all the fields found from the query.
+
+        Parameters
+        ----------
+        filename: str
+            the name where to save the dictionary output
+        fields: list
+            the fields returned from the filter function.
+        log: logging.Logger
+            the log object.
+        """
+        if not fields:
+            log.warning("No fields to display.")
+            return
+        dictionary_keys = fields[0].keys()
+        max_lengths = {}
+        header = ""
+        row_split = ""
+        for key in dictionary_keys:
+            max_lengths[key] = max(max([len(str(f[key])) for f in fields]), len(key))
+            header += f"{key:{max_lengths[key]}}|"
+            row_split += "-" * max_lengths[key] + "|"
+        with open(filename, 'a') as file:
+            print(header, file=file)
+            print(row_split, file=file)
+            for field in fields:
+                row = ""
+                for key in dictionary_keys:
+                    row += f"{str(field[key]):{max_lengths[key]}}|"
+                print(row, file=file)
 
 
 def main(args):
     log = setup_logger("dictionary_interactions", log_level=args.log_level)
-    global base_url
-    base_url = f"dwv-dictionary.{args.url}"
-
-    if args.key is None:
-        cert = args.cert
-    else:
-        cert = (args.cert, args.key)
-
-    res = get_entire_dictionary(cert, args.auths, args.data_types, args.namespace, args.output, log=log, use_ip=args.ip)
+    di = DictionaryInteractions(args, log)
+    res = di.get_entire_dictionary(args.auths, args.data_types, args.output)
     return res
 
 
